09f150cb555542474e54ce90e78c0d25b1db696b
Omer Uziel <80907784+OmerUzi@users.noreply.github.com>
2024-12-22 03:20:17 +0200

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -8,18 +8,8 @@ import intervals
 
 
 class Assignment2(object):
-    """Assignment 2 skeleton.
-
-    Please use these function signatures for this assignment and submit this file, together with the intervals.py.
-    """
 
     def sample_from_D(self, m):
-        """Sample m data samples from D.
-        Input: m - an integer, the size of the data sample.
-
-        Returns: np.ndarray of shape (m,2) :
-                A two dimensional array of size m that contains the pairs where drawn from the distribution P.
-        """
         # Create a list xs with m floats in [0,1]
         xs = np.sort(np.random.uniform(0, 1, m)).tolist()  # Convert the array to a list
 
@@ -34,19 +24,6 @@ class Assignment2(object):
 
 
     def experiment_m_range_erm(self, m_first, m_last, step, k, T):
-        """Runs the ERM algorithm.
-        Calculates the empirical error and the true error.
-        Plots the average empirical and true errors.
-        Input: m_first - an integer, the smallest size of the data sample in the range.
-               m_last - an integer, the largest size of the data sample in the range.
-               step - an integer, the difference between the size of m in each loop.
-               k - an integer, the maximum number of intervals.
-               T - an integer, the number of times the experiment is performed.
-
-        Returns: np.ndarray of shape (n_steps,2).
-            A two dimensional array that contains the average empirical error
-            and the average true error for each m in the range accordingly.
-        """
         empirical_errors = []
         true_errors = []
 
@@ -83,15 +60,6 @@ class Assignment2(object):
         
 
     def experiment_k_range_erm(self, m, k_first, k_last, step):
-        """Finds the best hypothesis for k= 1,2,...,10.
-        Plots the empirical and true errors as a function of k.
-        Input: m - an integer, the size of the data sample.
-               k_first - an integer, the maximum number of intervals in the first experiment.
-               m_last - an integer, the maximum number of intervals in the last experiment.
-               step - an integer, the difference between the size of k in each experiment.
-
-        Returns: The best k value (an integer) according to the ERM algorithm.
-        """
         empirical_errors = []
         true_errors = []
         xs, ys = self.sample_from_D(m)
@@ -120,16 +88,6 @@ class Assignment2(object):
         
 
     def experiment_k_range_srm(self, m, k_first, k_last, step):
-        """Run the experiment in (c).
-        Plots additionally the penalty for the best ERM hypothesis.
-        and the sum of penalty and empirical error.
-        Input: m - an integer, the size of the data sample.
-               k_first - an integer, the maximum number of intervals in the first experiment.
-               m_last - an integer, the maximum number of intervals in the last experiment.
-               step - an integer, the difference between the size of k in each experiment.
-
-        Returns: The best k value (an integer) according to the SRM algorithm.
-        """
         empirical_errors = []
         true_errors = []
         penalty_list = []
@@ -175,12 +133,6 @@ class Assignment2(object):
 
 
     def cross_validation(self, m):
-        """Finds a k that gives a good test error.
-        Input: m - an integer, the size of the data sample.
-
-        Returns: The best k value (an integer) found by the cross validation algorithm.
-        """
-       
         validation_errors = []
         xs, ys = self.sample_from_D(int(0.8*m))
         validation_xs, validation_ys = self.sample_from_D(int(0.2*m))
